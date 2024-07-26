f0ce1fff6ce0321572aa9e51f094987edc4a9f61
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-26 21:58:09 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+import pandas as pd
 
 st.header("Chatbot 1")
 
@@ -15,26 +16,26 @@ def add_question():
 
 with tab1:
     st.subheader("Add Questions to your list")
-    # del st.session_state.questions[-1]
-    i = 1
-    # del st.session_state.questions[-1]
-    question, answer = add_question()
-    if st.button("Add", use_container_width=1):
-        if question == "" or answer == "":
-            st.error("Enter your data!")
-        else:
-            exist = False
-            for i in st.session_state.questions:
-                if i['question'] == question:
-                    st.error("Question already exist!")
-                    exist = True
-                    break
-            if exist == False:
-                st.session_state.questions.append({"question": question, "answer": answer})
-                st.success("Success")
-                time.sleep(1)
-                st.rerun()
-    st.write("---")
+    with st.form("test form"):
+        question = st.text_input("Question", placeholder="اكتب هنا السؤال")
+        answer = st.text_input("Answer", placeholder="اكتب هنا الاجابة")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            i = 1
+            if question == "" or answer == "":
+                st.error("Enter your data!")
+            else:
+                exist = False
+                for i in st.session_state.questions:
+                    if i['question'] == question:
+                        st.error("Question already exist!")
+                        exist = True
+                        break
+                if exist == False:
+                    st.session_state.questions.append({"question": question, "answer": answer})
+                    st.success("Success")
+                    time.sleep(1)
+                    st.rerun()
     if len(st.session_state.questions) == 0:
         st.info("No questions found!")
     else:
@@ -52,69 +53,72 @@ with tab1:
         st.write("---")
         if st.button("Finish", use_container_width=1):
             st.success("success")
-
 with tab2:
-    st.subheader("Manage Question")
+    st.columns([5,1])[0].subheader("Manage Question")
     if len(st.session_state.questions) == 0:
         st.info("No questions have found!")
     else:
         i = 1
         choices = []
+        questions = []
         for r in st.session_state.questions:
             if r["question"] != "":
-                choices.append(st.checkbox(f"**Question no. {i}:**  \nQ: {r['question']}  \nA: {r['answer']}"))
+                questions.append({"Number":f"Q {i}", "Question":r['question'], "Answer":r['answer'], "Select":False})
                 i += 1
-        if st.button("Remove"):
-            if True not in choices:
+        df = pd.DataFrame(questions)
+        edited = pd.DataFrame(st.data_editor(df, hide_index=True, use_container_width=1, disabled=("Number",)))
+        col1, col2, col3 = st.columns([1,1,5])
+        with col1:
+            remove = st.button("Remove")
+        with col2:
+            update = st.button("Update")
+        with col3:
+            reset = st.button("Reset Changes")
+        if reset:
+            st.rerun()
+        if remove:
+            if True not in edited["Select"].values:
                 st.error("No chooses found!")
             else:
                 i = 0
-                for c in choices:
+                for c in edited["Select"].values:
                     if c:
                         del st.session_state.questions[i]
                     i +=1
-                st.success("Question Removed")
+                st.session_state.sure = False
+                st.success("Question Removed Successfully")
                 time.sleep(1)
                 st.rerun()
-        # Edit
-        # if "yes" not in st.session_state:
-        #     st.session_state.yes = False
-        # q = a = ""
-        # if st.button("Edit", use_container_width=1):
-        #     if True not in choices:
-        #         st.error("No choices found!")
-        #     else:
-        #         st.session_state.yes = True
-        #         i, j = 0, 0
-        #         for c in choices:
-        #             if c:
-        #                 q = st.session_state.questions[i]['question']
-        #                 a = st.session_state.questions[i]['answer']
-        #                 j = i
-        #             i += 1
-        # if st.session_state.yes == True:
-        #     question = st.text_input(f"Edit Question", value=q)
-        #     answer = st.text_input(f"Edit Answer", value=a)
-        #     if st.button("Finish"):
-        #         st.session_state.questions[j] = {"question": question, "answer": answer}
-        #         st.success("Question Edited")
-        #         st.session_state.yes == False
-        #         time.sleep(1)
-        #         st.rerun()
+        if update:
+            if True not in edited["Select"].values:
+                st.error("No chooses found!")
+            else:
+                edit = False
+                i = 0
+                for c in range(len(edited.values)):
+                    if edited.values[c][3]:
+                        if edited.values[c][1] == "":
+                            st.error(f"Question is empty!")
+                            time.sleep(2)
+                            st.rerun()
+                        if df.values[c][1] != edited.values[c][1]:
+                            if edited.values[c][1] not in df["Question"].values:
+                                st.session_state.questions[c]['question'] = edited.values[c][1]
+                                edit = True
+                            else:
+                                st.error(f"Question **:{edited.values[c][1]}:** is already exist!")
+                                time.sleep(2)
+                                st.rerun()
+                        if df.values[c][2] != edited.values[c][2]:
+                            st.session_state.questions[c]['answer'] = edited.values[c][2]
+                            edit = True
+                    i +=1
+                if edit ==True:
+                    st.session_state.sure = False
+                    st.success("Question Updated Successfully")
+                    time.sleep(2)
+                    st.rerun()
+                else:
+                    st.error("No changes found!")
 with tab3:
     st.columns([1,2,1])[1].image("images/Chat bot-pana.png")
-    # st.subheader("Test your **bot**")
-    # l = []
-    # happened = 0
-    # for i in st.session_state.questions:
-    #     l.append(st.button(i['question'], use_container_width=True))
-    #     if True in l and happened==0:
-    #         for i in range(len(l)):
-    #             if l[i]:
-    #                 st.info(f"{st.session_state.questions[i]['answer']}")
-    #                 happened = 1
-    # for i in range(len(l)):
-    #     if l[i]:
-    #         st.info(f"{st.session_state.questions[i]['answer']}")
-# if st.button("Add Quesion"):
-#     add_question()
