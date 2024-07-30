eb32be4398ecdc8285fe2f1486c8cd6919a62348
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-07-30 21:12:59 +0300

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,7 +2,7 @@ import streamlit as st
 # st.columns([1,2,1])[1].image("images\logo.png")
 # with st.columns(3)[1]:
 #     st.title("I CARE")
-st.columns([1,2,1])[1].image("images\logo.png")
+st.columns([1,2,1])[1].image("images/logo.png")
 st.info("Easy Build **Chatbots** for your business")
 st.write("  \n")
 
