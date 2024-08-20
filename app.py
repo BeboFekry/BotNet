3160cc24bc5ba056eba5ce992f2c3b0ce3ef7c21
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-20 21:30:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # primaryColor="#4b84ff"
 # st.set_page_config()
 
-st.set_page_config(page_title="BotNet", icon='images/icon2.png', layout="wide")
+st.set_page_config(page_title="BotNet", page_icon='images/icon2.png', layout="wide")
 
 st.logo("images/Picsart_24-08-15_01-45-01-740.png")
 
